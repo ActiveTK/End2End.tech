2baf47c6425f74a70dc47d9bea397b64f734060e
ActiveTK <68322433+ActiveTK@users.noreply.github.com>
2023-08-27 21:19:35 +0900

    フッターを編集

--- scripts/home_loader.php
+++ scripts/home_loader.php
@@ -81,57 +81,52 @@
       <footer class="mx-auto max-w-screen-2xl px-4 md:px-8">
         <div class="mb-16 grid grid-cols-2 gap-12 pt-10 md:grid-cols-4 lg:grid-cols-6 lg:gap-8 lg:pt-12">
           <div class="col-span-full lg:col-span-2">
-
             <div class="mb-4 lg:-mt-2 gap-2 text-xl font-bold text-gray-100 md:text-2xl">
               End2End.tech
             </div>
-
             <p class="mb-6 text-gray-400 sm:pr-8">End2End暗号化対応・ノーログで、安心安全のオープンソースの匿名ファイルアップロードサービス。</p>
-
           </div>
 
           <div>
             <div class="mb-4 font-bold uppercase tracking-widest text-gray-100">Sitemap</div>
-
             <nav class="flex flex-col gap-4">
               <div>
                 <a href="/about" class="text-gray-400 transition duration-100 hover:text-indigo-500 active:text-indigo-600">サイト概要</a>
               </div>
-
               <div>
                 <a href="/license" class="text-gray-400 transition duration-100 hover:text-indigo-500 active:text-indigo-600">利用規約</a>
               </div>
-
               <div>
                 <a href="/contact" class="text-gray-400 transition duration-100 hover:text-indigo-500 active:text-indigo-600">お問い合わせ</a>
-              </div>
-
-              <div>
-                <a href="/sitemap.xml" class="text-gray-400 transition duration-100 hover:text-indigo-500 active:text-indigo-600">sitemap.xml</a>
               </div>
             </nav>
           </div>
 
           <div>
             <div class="mb-4 font-bold uppercase tracking-widest text-gray-100">Development</div>
-
             <nav class="flex flex-col gap-4">
               <div>
-                <a href="https://github.com/ActiveTK/End2End.tech" class="text-gray-400 transition duration-100 hover:text-indigo-500 active:text-indigo-600">Github</a>
+                <a href="https://github.com/ActiveTK/End2End.tech" class="text-gray-400 transition duration-100 hover:text-indigo-500 active:text-indigo-600">Githubリポジトリ</a>
+              </div>
+              <div>
+                <a href="https://profile.activetk.jp/" class="text-gray-400 transition duration-100 hover:text-indigo-500 active:text-indigo-600">開発者</a>
               </div>
 
               <div>
-                <a href="https://profile.activetk.jp/" class="text-gray-400 transition duration-100 hover:text-indigo-500 active:text-indigo-600">Developer</a>
-              </div>
-
-              <div>
-                <a href="https://github.com/ActiveTK/End2End.tech/blob/main/LICENSE" class="text-gray-400 transition duration-100 hover:text-indigo-500 active:text-indigo-600">License</a>
+                <a href="https://github.com/ActiveTK/End2End.tech/blob/main/LICENSE" class="text-gray-400 transition duration-100 hover:text-indigo-500 active:text-indigo-600">The MIT License</a>
               </div>
             </nav>
           </div>
 
+          <div>
+            <div class="mb-4 font-bold uppercase tracking-widest text-gray-100">et cetera</div>
+            <nav class="flex flex-col gap-4">
+              <div>
+                <a href="/sitemap.xml" class="text-gray-400 transition duration-100 hover:text-indigo-500 active:text-indigo-600">sitemap.xml</a>
+              </div>
+            </nav>
+          </div>
         </div>
-
         <div class="border-t border-gray-800 py-8 text-center text-sm text-gray-400">(c) 2023 ActiveTK.</div>
       </footer>
     </div>
