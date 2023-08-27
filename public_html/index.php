64b5095638994a7dfa970f6b07008494ff96699a
ActiveTK <68322433+ActiveTK@users.noreply.github.com>
2023-08-27 19:08:11 +0900

    最低限の基本機能を追加
    必須ヘッダーの出力やHTTPSリダイレクト、パスの取得、出力したHTMLの圧縮などを実装しました。

--- public_html/index.php
+++ public_html/index.php
@@ -1,1 +1,101 @@
-just setting up my end2end dot tech
+<?php
+
+  /*!
+   * End2End.tech - Anonymous File Uploader
+   * (c) 2023 ActiveTK.
+   * Released under the MIT license.
+   */
+
+  // 読み込み開始時刻を記録
+  define( "LOAD_START_TIME", microtime( true ) );
+
+  // HTTPSの自動リダイレクト処理
+  if ( empty( $_SERVER['HTTPS'] ) ) {
+    header( "Location: https://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}" );
+    die();
+  }
+
+  // 必須ヘッダーの出力
+  header( "X-Frame-Options: deny" );
+  header( "Access-Control-Allow-Origin:" );
+  header( "Strict-Transport-Security: max-age=63072000; preload" );
+  header( "X-XSS-Protection: 1; mode=block" );
+  header( "X-Content-Type-Options: nosniff" );
+  header( "X-Permitted-Cross-Domain-Policies: none" );
+  header( "Referrer-Policy: same-origin" );
+
+  // パスの取得
+  // リクエストURIから先頭のスラッシュを削除したもの
+  // ex: (GET /hoge/index.php HTTP/1.1) => request_path === "hoge/index.php"
+  define( "request_path", ( isset( $_GET["request"] ) && is_string( $_GET["request"] ) ) ? strtolower( $_GET["request"] ) : "" );
+
+  // パスの末尾がスラッシュである場合の処理
+  if ( strlen( request_path ) > 0 && substr( request_path, -1 ) == "/" ) {
+    header( "Location: https://end2end.tech/" . strtolower( substr( $_GET["request"], 0, -1 ) ), true, 301 );
+    exit();
+  }
+
+  // HTMLを圧縮する関数
+  function sanitize_output($buffer) {
+
+    // XSS攻撃を防止するランダムな英数列
+    define( "FLG", substr( base_convert( sha1( md5( uniqid() ) . md5 ( microtime() ) ), 16, 36), 0, 5) );
+
+    // Content-TypeがHTMLでない場合の処理
+    foreach(headers_list() as $line)
+    {
+      list($title, $data) = explode(": ", $line, 2);
+      if (strtolower($title) == "content-type" && false === strpos($data, "text/html"))
+        return $buffer;
+    }
+
+    // 中身の改行が意味を持つタグの処理
+    $buffer = preg_replace_callback("/<pre.*?<\/pre>/is", function($matches) {
+      return "_" . FLG . "_here___prf__start" . base64_encode(urlencode($matches[0])) . "_" . FLG . "_here___prf__end";
+    }, $buffer);
+    $buffer = preg_replace_callback("/<script.*?<\/script>/is", function($matches) {
+      return "_" . FLG . "_here___sct__start" . base64_encode(urlencode($matches[0])) . "_" . FLG . "_here___sct__end";
+    }, $buffer);
+    $buffer = preg_replace_callback("/<textarea.*?<\/textarea>/is", function($matches) {
+      return "_" . FLG . "_here___txs__start" . base64_encode(urlencode($matches[0])) . "_" . FLG . "_here___txs__end";
+    }, $buffer);
+
+    // 改行や空白を削除
+    $buffer = preg_replace(array("/\>[^\S]+/s", "/[^\S]+\</s", "/(\s)+/s" ), array(">", "<", " "), $buffer);
+
+    // 改行が意味を持つタグを元に戻す処理
+    $buffer = preg_replace_callback("/_" . FLG . "_here___prf__start.*?_" . FLG . "_here___prf__end/is", function($matches) {
+      return urldecode(base64_decode(substr(substr($matches[0], 24), 0, -22)));
+    }, $buffer);
+    $buffer = preg_replace_callback("/_" . FLG . "_here___sct__start.*?_" . FLG . "_here___sct__end/is", function($matches) {
+      return urldecode(base64_decode(substr(substr($matches[0], 24), 0, -22)));
+    }, $buffer);
+    $buffer = preg_replace_callback("/_" . FLG . "_here___txs__start.*?_" . FLG . "_here___txs__end/is", function($matches) {
+      return urldecode(base64_decode(substr(substr($matches[0], 24), 0, -22)));
+    }, $buffer);
+
+    // DOCTYPE宣言の後にコメントを追加
+    if (substr($buffer, 0, 15) == "<!DOCTYPE html>")
+      $buffer = substr($buffer, 15);
+
+    return
+      "<!DOCTYPE html><!--\n" .
+        "\n" .
+        "  End2End.tech / (c) 2023 ActiveTK.\n\n" .
+        "  Server-Side Time: " . ( microtime( true ) - LOAD_START_TIME ) . "s\n" .
+        "  Cached Date: " . ( new DateTime( "now", new DateTimeZone( "GMT" ) ) ) -> format( "Y-m-d H:i:sP" ) . "\n" .
+      "\n-->" . $buffer . "\n";
+
+    return $buffer . "\n";
+  }
+
+  // 出力したバッファーを自動で圧縮するように設定
+  ob_start( "sanitize_output" );
+
+  // ホーム
+  if ( empty( request_path ) ) {
+    exit( "just setting up my end2end dot tech" );
+  }
+
+  header( "HTTP/1.1 404 Not Found" );
+  die( "HTTP 404 - Not Found" );
