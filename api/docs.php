e23742b5217c82b72a712953eac265617d76bec2
ActiveTK <68322433+ActiveTK@users.noreply.github.com>
2023-08-28 23:53:38 +0900

    APIのドキュメントを作成しました。

---
+++ api/docs.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+    <title>API - End2End.tech</title>
+    <meta name="robots" content="All">
+    <meta name="description" content="APIのドキュメンテーションです。">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://www.activetk.jp/js/prism.js"></script>
+    <script src="https://end2end.tech/js/min.js"></script>
+    <link href="https://www.activetk.jp/css/prism.css" rel="stylesheet">
+    <link href="https://end2end.tech/css/download.css" rel="stylesheet">
+  </head>
+  <body>
+
+    <br>
+    <h1 class="text-3xl font-bold" align="center">
+      API - End2End.tech
+    </h1>
+    <br>
+
+    <noscript>
+      <div align="center">
+        <h1>このページを表示するには、JavaScriptを有効化して下さい。</h1>
+      </div>
+    </noscript>
+
+    <div id="main" class="main py-6 sm:py-8 lg:py-12">
+      <div class="mx-auto max-w-screen-md px-4 md:px-8">
+
+        <p class="mb-6 sm:text-lg md:mb-8">
+          End2End.techでは、外部のスクリプトやコマンドラインから簡単にファイルをアップロードできるAPIを用意しています。
+        </p>
+
+        <h1 class="textblue mb-2 text-xl font-semibold sm:text-2xl md:mb-4">ファイルの新規アップロード</h1>
+        <p class="mb-6 sm:text-lg md:mb-8">
+          ファイルを新規にアップロードするには、以下のURLにPOSTリクエストを送信して下さい。
+
+        </p>
+
+      </div>
+    </div>
+
+    <?php require_once("../scripts/footer.php"); ?>
+
+  </body>
+</html>
