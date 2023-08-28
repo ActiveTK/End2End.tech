19027c094be0acf41cd4bf1947974b711d85c5a2
ActiveTK <68322433+ActiveTK@users.noreply.github.com>
2023-08-28 10:02:41 +0900

    Aboutページを作成

--- scripts/about.php
+++ scripts/about.php
@@ -4,18 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
-    <title>End2End.tech - Anonymous File Uploader</title>
+    <title>About - End2End.tech</title>
     <meta name="robots" content="All">
-    <meta name="description" content="匿名で簡単にファイルをアップロードできます。End2End暗号化対応・ノーログで、安心安全のオープンソースです。">
+    <meta name="description" content="サイト概要です。">
     <script src="https://cdn.tailwindcss.com"></script>
-    <script src="/js/index.js"></script>
-    <link href="/css/index.css" rel="stylesheet">
+    <script src="/js/about.js"></script>
+    <link href="/css/about.css" rel="stylesheet">
   </head>
   <body>
 
     <br>
     <h1 class="text-3xl font-bold" align="center">
-      End2End.tech - Anonymous File Uploader
+      About - End2End.tech
     </h1>
     <br>
 
@@ -27,6 +27,92 @@
 
     <div id="main" class="main py-6 sm:py-8 lg:py-12">
       
+      <div class="bg-white py-6 sm:py-8 lg:py-12">
+        <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
+
+          <div class="mb-10 md:mb-16">
+            <h2 class="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">簡単に使える匿名ファイルアップローダー</h2>
+            <p class="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">本サイトは、以下のような機能を持つ匿名のファイルアップロードサービスです。</p>
+          </div>
+
+          <div class="grid gap-8 sm:grid-cols-2 md:gap-12 xl:grid-cols-3 xl:gap-16">
+            <div class="flex gap-4 md:gap-6">
+              <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
+                </svg>
+              </div>
+              <div>
+                <h3 class="mb-2 text-lg font-semibold md:text-xl">No log</h3>
+                <p class="mb-2 text-gray-500">本サイトでは、ユーザーのIPアドレスをサーバー上に記録していません。ただし、サイバー攻撃防止のため、30日間はIPアドレスを省いたHTTPリクエストの履歴が保存されます。</p>
+              </div>
+            </div>
+
+            <div class="flex gap-4 md:gap-6">
+              <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z" />
+                </svg>
+              </div>
+              <div>
+                <h3 class="mb-2 text-lg font-semibold md:text-xl">E2E暗号化対応</h3>
+                <p class="mb-2 text-gray-500">パスワードによるファイルのエンドツーエンド暗号化に対応し、安全にファイルを送受信できます。</p>
+              </div>
+            </div>
+
+            <div class="flex gap-4 md:gap-6">
+              <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z" />
+                </svg>
+              </div>
+              <div>
+                <h3 class="mb-2 text-lg font-semibold md:text-xl">1ファイル100MBまで</h3>
+                <p class="mb-2 text-gray-500">現在、1ファイル100MBまでアップロードが可能です。この制限は、今後ユーザー数が増えた場合に緩和される場合があります。</p>
+              </div>
+            </div>
+
+            <div class="flex gap-4 md:gap-6">
+              <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
+                </svg>
+              </div>
+              <div>
+                <h3 class="mb-2 text-lg font-semibold md:text-xl">CDNからの高速ダウンロード</h3>
+                <p class="mb-2 text-gray-500">最も近いCDNサーバーと、高速でファイルをアップロード/ダウンロードできます。</p>
+              </div>
+            </div>
+
+            <div class="flex gap-4 md:gap-6">
+              <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 5.636l-3.536 3.536m0 5.656l3.536 3.536M9.172 9.172L5.636 5.636m3.536 9.192l-3.536 3.536M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-5 0a4 4 0 11-8 0 4 4 0 018 0z" />
+                </svg>
+              </div>
+              <div>
+                <h3 class="mb-2 text-lg font-semibold md:text-xl">End2End.tech API</h3>
+                <p class="mb-2 text-gray-500">専用のAPIを用いて外部からファイルをアップロードすることができます。</p>
+                <a href="https://api.activetk.jp" class="font-bold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700">詳細</a>
+              </div>
+            </div>
+
+            <div class="flex gap-4 md:gap-6">
+              <div class="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-indigo-500 text-white shadow-lg md:h-14 md:w-14 md:rounded-xl">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+                </svg>
+              </div>
+              <div>
+                <h3 class="mb-2 text-lg font-semibold md:text-xl">安全安心のオープンソース</h3>
+                <p class="mb-2 text-gray-500">本サイトは完全オープンソースで開発が行われ、誰でもIssueやPull Requestを送信できます。</p>
+                <a href="https://github.com/ActiveTK/End2End.tech" class="font-bold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700">詳細</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
 
     <?php require_once("../scripts/footer.php"); ?>
