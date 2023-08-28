ade943a853710536a59ec61e373657b6b5b70580
ActiveTK <68322433+ActiveTK@users.noreply.github.com>
2023-08-28 22:58:54 +0900

    利用規約ページを作成

--- scripts/license.php
+++ scripts/license.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
-    <title>End2End.tech - Anonymous File Uploader</title>
+    <title>利用規約 - End2End.tech</title>
     <meta name="robots" content="All">
-    <meta name="description" content="匿名で簡単にファイルをアップロードできます。End2End暗号化対応・ノーログで、安心安全のオープンソースです。">
+    <meta name="description" content="利用規約です。">
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="/js/index.js"></script>
     <link href="/css/index.css" rel="stylesheet">
@@ -15,7 +15,7 @@
 
     <br>
     <h1 class="text-3xl font-bold" align="center">
-      End2End.tech - Anonymous File Uploader
+      利用規約 - End2End.tech
     </h1>
     <br>
 
@@ -26,7 +26,44 @@
     </noscript>
 
     <div id="main" class="main py-6 sm:py-8 lg:py-12">
-      
+      <div class="mx-auto max-w-screen-md px-4 md:px-8">
+
+        <p class="mb-6 sm:text-lg md:mb-8">2023年8月28日 制定</p>
+
+        <h1 class="textblue mb-2 text-xl font-semibold sm:text-2xl md:mb-4">第2条 サービスの内容</h1>
+        <p class="mb-6 sm:text-lg md:mb-8">
+          本サービスは、ユーザーがアップロードしたファイルをホストするものです。<br>
+          インターネットへ接続できる回線や端末、ブラウザ等はお客様が用意していただく必要がございます。
+        </p>
+
+        <h1 class="textblue mb-2 text-xl font-semibold sm:text-2xl md:mb-4">第3条 禁止事項</h1>
+        <p class="mb-6 sm:text-lg md:mb-8">
+          本サービスにおいて、以下の行為を禁止します。<br>
+          法令あるいは禁止事項に抵触する行為が発覚した場合には、然るべき処置を取ります。
+        </p>
+
+        <li>データを共有するユーザーは、一般大衆に向けて公開するのに相応しくないデータのアップロードを行ってはなりません。</li>
+        <li>データを共有するユーザーは、法律上、所持が禁止されているデータ(児童ポルノ等)のアップロードを行ってはなりません。</li>
+        <li>データを共有するユーザーは、第三者の権利(著作権等)を侵害するコンテンツのアップロードを行ってはなりません。</li>
+        <li>データを共有するユーザーは、犯罪を助長し、あるいは加担する行為に関連する一切のコンテンツのアップロードを行ってはなりません。</li>
+        <li>データをダウンロードするユーザーは、ダウンロードしたデータが禁止事項に抵触していることが明らかなとき、それをすぐに削除しなければなりません。</li>
+        <li>サーバーに故意に負荷をかける行為(DDoS攻撃等)を行ってはなりません。</li>
+
+        <h1 class="textblue mb-2 text-xl font-semibold sm:text-2xl md:mb-4">第5条 免責事項</h1>
+        <p class="mb-6 sm:text-lg md:mb-8">
+          本利用規約に違反するデータは、データ販売者の意思によらず消去します。<br>
+          この場合、データ消去に際して発生した一切の問題について、これをお客様自身で解決しなければなりません。<br>
+          お客様の過失又は故意若しくは悪用によって発生した全ての問題や第三者様とのトラブル、エラーに関する責任に対し、<br>
+          自己の費用と責任をもってこれを解決するものとし、 管理者、開発者並びに開発関係者は一切の責任を負わないものとします。
+        </p>
+
+        <h1 class="textblue mb-2 text-xl font-semibold sm:text-2xl md:mb-4">第7条 サービス停止</h1>
+        <p class="mb-6 sm:text-lg md:mb-8">
+          管理者側の一方的な都合で本サービスの提供を停止した場合や、本利用規約の定める禁止事項に抵触した事によって本サービスご利用できなくなった事により発生した、<br>
+          全ての問題やトラブル、エラーに関する責任に対し、管理者、開発者並びに開発関係者は一切の責任を負わないものとします。
+        </p>
+
+      </div>
     </div>
 
     <?php require_once("../scripts/footer.php"); ?>
