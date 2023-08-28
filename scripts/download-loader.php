2a62ee4c4c68d60f0d3f83d903fb585352390bb9
ActiveTK <68322433+ActiveTK@users.noreply.github.com>
2023-08-28 19:07:28 +0900

    パスワードの入力場所を作成

--- scripts/download-loader.php
+++ scripts/download-loader.php
@@ -105,10 +105,12 @@
           <?php if ( defined( "DownloadBlocked" ) ) { ?>
             <?=DownloadBlocked?>
           <?php } else { ?>
-            u r able 2download.
             <?php if ( defined( "DataEncrypted" ) ) { ?>
-              btw, maybe file is encrypted.. enter the pwd plz!
+              ファイルはE2E暗号化されており、複合化にはパスワードが必要です。<br>
+              パスワード:
+               <input id="password" class="rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" placeholder="p@ssword" /><br>
             <?php } ?>
+            <input type="button" id="downloadData" class="inline-block rounded-lg bg-indigo-500 px-8 py-3 text-center text-sm font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-600 focus-visible:ring active:bg-indigo-700 md:text-base" value="ファイルをダウンロード">
           <?php } ?>
 
         </p>
