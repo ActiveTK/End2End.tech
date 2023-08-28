8339cd58f3eb2027cc711c4f97e496b7eed049f9
ActiveTK <68322433+ActiveTK@users.noreply.github.com>
2023-08-28 23:21:57 +0900

    UIを編集しました。

--- scripts/contact.php
+++ scripts/contact.php
@@ -31,11 +31,11 @@
         <h1 class="textblue mb-2 text-xl font-semibold sm:text-2xl md:mb-4"></h1>
         <p class="mb-6 sm:text-lg md:mb-8">
           <form action="" enctype="multipart/form-data" method="post">
-            <input type="text" class="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" name="title" maxlength="120" placeholder="ここにタイトルを入力してください(120文字まで)" required>
-            <br>
-            <input type="email" class="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" name="email" maxlength="120" placeholder="メールアドレスを入力して下さい" required>
-            <br>
-            <textarea name="data" maxlength="1080" placeholder="ここに内容を入力してください(1080文字まで)" class="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" required></textarea>
+            <input type="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="title" maxlength="120" placeholder="ここにタイトルを入力してください(120文字まで)" required>
+            <br><br>
+            <input type="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="email" maxlength="120" placeholder="メールアドレスを入力して下さい" required>
+            <br><br>
+            <textarea name="data" maxlength="1080" placeholder="ここに内容を入力してください(1080文字まで)" class="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring" style="height:400px;" required></textarea>
             <br>
             <input type="submit" value="送信" style="width:73px;height:33px;background-color:#90ee90;">
           </form>
