cd85a6c81f7ce41e7d573c983915676dd05160d8
ActiveTK <68322433+ActiveTK@users.noreply.github.com>
2023-08-29 16:23:58 +0900

    アップロードしたファイル一覧のUIを編集

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -200,7 +200,7 @@
 
         var filename = document.createTextNode(FileDetails["FileName"]);
         filename.className = "px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white";
-        e.className = "bg-white border-b dark:bg-gray-800 dark:border-gray-700 px-6 py-3";
+        e.className = "height40 bg-white border-b dark:bg-gray-800 dark:border-gray-700 px-6 py-3";
         e.insertCell(0).appendChild(filename);
 
         var anchor = document.createElement('a');
