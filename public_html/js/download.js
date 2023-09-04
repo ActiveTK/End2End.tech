c86f27ea97f8305cf124124f0803344a8b9d5304
ActiveTK <68322433+ActiveTK@users.noreply.github.com>
2023-09-04 20:48:01 +0900

    Revert "asyncを追加"
    This reverts commit e8979cfb7cd535dd222b88fc1b51a7f2e426d75f.

--- public_html/js/download.js
+++ public_html/js/download.js
@@ -9,7 +9,7 @@
 
         _("main").style.display = "block";
 
-		_("downloadData").onclick = async function () {
+		_("downloadData").onclick = function () {
 			if (window.end2endtech.Encrypted === true) {
 
 				if (window.decryptionarray) {
