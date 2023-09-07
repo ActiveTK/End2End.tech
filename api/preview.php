8cf7d24ee1537b8497d010427c12ad54fff8aa05
ActiveTK <68322433+ActiveTK@users.noreply.github.com>
2023-09-07 21:20:18 +0900

    構文エラーを修正

--- api/preview.php
+++ api/preview.php
@@ -24,7 +24,7 @@
       "Salt" => substr($fileheader, 0, 32),
       "IV" => substr($fileheader, 33, 32),
       "Mode" => "CryptoJS.mode.CBC",
-      "Data" => substr($fileheader, 66) . "... (省略されました)";
+      "Data" => substr($fileheader, 66) . "... (省略されました)"
     ), JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT );
     echo htmlspecialchars( $text, ENT_SUBSTITUTE, 'UTF-8' );
     echo "</pre></body></html>";
