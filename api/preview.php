773dc9ea6ecff618631494b582f3d19f32d4d462
ActiveTK <68322433+ActiveTK@users.noreply.github.com>
2023-09-07 21:09:34 +0900

    暗号化されたファイルのプレビューを変更

--- api/preview.php
+++ api/preview.php
@@ -5,6 +5,33 @@
   $basepath = "../../objects/blob/";
 
   if ( isset( $_GET["id"] ) && is_string( $_GET["id"] ) &&
+       isset( $_GET["encrypted"] ) && is_string( $_GET["encrypted"] ) ) {
+
+    $_GET["id"] = basename( $_GET["id"] );
+
+    if ( !ctype_alnum( $_GET["id"] ) )
+      die();
+
+    $File = sys_get_temp_dir() . "/end2endtech_" . $_GET["id"];
+    if ( !file_exists( $File ) )
+      die();
+
+    header( "Content-Type: text/html" );
+    echo "<html><head><meta name=\"robots\" content=\"noindex, nofollow\"><meta name=\"color-scheme\" content=\"dark light\"></head><body style=\"background-color:#181818;color:#ffffff;\"><pre style=\"word-wrap: break-word; white-space: pre-wrap;\">";
+    $fileheader = file_get_contents( $File, FALSE, NULL, 0, 1024 );
+    $text = json_encode( array(
+      "Encrypted" => true,
+      "Salt" => substr($fileheader, 0, 32),
+      "IV" => substr($fileheader, 33, 65),
+      "Mode" => "CryptoJS.mode.CBC",
+      "Data" => substr($fileheader, 66) . "...",
+    ), JSON_UNESCAPED_UNICODE|JSON_PRETTY_PRINT );
+    echo htmlspecialchars( $text ), ENT_SUBSTITUTE, 'UTF-8' );
+    echo "</pre></body></html>";
+    exit();
+  }
+
+  if ( isset( $_GET["id"] ) && is_string( $_GET["id"] ) &&
        isset( $_GET["type"] ) && is_string( $_GET["type"] ) ) {
 
     $_GET["id"] = basename( $_GET["id"] );
--- scripts/preview.php
+++ scripts/preview.php
@@ -9,84 +9,90 @@
 
   if ( !defined( "DownloadBlocked" ) && !defined( "EndtoEndEncrypted" ) ) {
 
-    $PathInfo = pathinfo( FileInfo["FileName"] );
-    if ( !isset( $PathInfo["extension"] ) )
-      $PathInfo["extension"] = "";
+    if ( !empty( FileInfo["EndtoEndEncrypted"] ) && FileInfo["EndtoEndEncrypted"] == "true" ) {
+      echo "<iframe style='width:604px;height:340px;' src='" . APIEndPoint . "preview" . ( EnableAPIAsSubDomain ? "?" : "&" ) . "id=" . FileInfo["FileID"] . "&encrypted'></iframe><br>";
+    } else {
 
-    $Ext = strtolower( $PathInfo["extension"] );
+      $PathInfo = pathinfo( FileInfo["FileName"] );
+      if ( !isset( $PathInfo["extension"] ) )
+        $PathInfo["extension"] = "";
 
-    if ( $Ext == "png" || $Ext == "jpeg" || $Ext == "jpg" || $Ext == "gif" ) {
+      $Ext = strtolower( $PathInfo["extension"] );
 
-      if ( $Ext == "jpeg" )
-        $Ext = "jpg";
+      if ( $Ext == "png" || $Ext == "jpeg" || $Ext == "jpg" || $Ext == "gif" ) {
 
-      if ( FileInfo["FileSize"] < 1024 * 1024 * 10 ) {
+        if ( $Ext == "jpeg" )
+          $Ext = "jpg";
 
-        $tempImage = sys_get_temp_dir() . "/end2endtech_" . FileInfo["FileID"];
-        if ( !file_exists( $tempImage ) ) {
-          $def = gzinflate( file_get_contents( $basepath . FileInfo["FileID"] ) );
-          file_put_contents( $tempImage, $def );
-          $size = getimagesizefromstring( $def );
-        } else {
-          $size = getimagesize( $tempImage );
-        }
+        if ( FileInfo["FileSize"] < 1024 * 1024 * 10 ) {
 
-        if ( $size ) {
+          $tempImage = sys_get_temp_dir() . "/end2endtech_" . FileInfo["FileID"];
+          if ( !file_exists( $tempImage ) ) {
+            $def = gzinflate( file_get_contents( $basepath . FileInfo["FileID"] ) );
+            file_put_contents( $tempImage, $def );
+            $size = getimagesizefromstring( $def );
+          } else {
+            $size = getimagesize( $tempImage );
+          }
 
-          list($width, $height, $type, $attr) = $size;
+          if ( $size ) {
 
-          if( $width >= 800 || $height >= 800 ) {
+            list($width, $height, $type, $attr) = $size;
 
-            if ( $type == IMAGETYPE_JPEG ) {
-              $original_image = imagecreatefromjpeg( $tempImage );
-              $newW = 600;
-              $newH = $newW * ($height / $width);
-              $newImg = imagecreatetruecolor( $newW, $newH );
-              $success = imagecopyresampled( $newImg, $original_image, 0, 0, 0, 0, $newW, $newH, $width, $height );
-              if ( $success ) {
-                imagejpeg( $newImg, $tempImage, 60 );
-                $width = $newW;
-                $height = $newH;
+            if( $width >= 800 || $height >= 800 ) {
+
+              if ( $type == IMAGETYPE_JPEG ) {
+                $original_image = imagecreatefromjpeg( $tempImage );
+                $newW = 600;
+                $newH = $newW * ($height / $width);
+                $newImg = imagecreatetruecolor( $newW, $newH );
+                $success = imagecopyresampled( $newImg, $original_image, 0, 0, 0, 0, $newW, $newH, $width, $height );
+                if ( $success ) {
+                  imagejpeg( $newImg, $tempImage, 60 );
+                  $width = $newW;
+                  $height = $newH;
+                }
               }
-            }
-            else if ( $type == IMAGETYPE_PNG ) {
-              $original_image = imagecreatefrompng( $tempImage );
-              $newW = 600;
-              $newH = $newW * ($height / $width);
-              $newImg = imagecreatetruecolor( $newW, $newH );
-              $success = imagecopyresampled( $newImg, $original_image, 0, 0, 0, 0, $newW, $newH, $width, $height );
-              if ( $success ) {
-                imagepng( $newImg, $tempImage, 6 );
-                $width = $newW;
-                $height = $newH;
+              else if ( $type == IMAGETYPE_PNG ) {
+                $original_image = imagecreatefrompng( $tempImage );
+                $newW = 600;
+                $newH = $newW * ($height / $width);
+                $newImg = imagecreatetruecolor( $newW, $newH );
+                $success = imagecopyresampled( $newImg, $original_image, 0, 0, 0, 0, $newW, $newH, $width, $height );
+                if ( $success ) {
+                  imagepng( $newImg, $tempImage, 6 );
+                  $width = $newW;
+                  $height = $newH;
+                }
+
               }
 
             }
+
+            echo "<div><img src='" . APIEndPoint . "preview" . ( EnableAPIAsSubDomain ? "?" : "&" ) . "id=" . FileInfo["FileID"] . "&type=" . $Ext . "' style='height:400px;width:auto;'></div><br>";
 
           }
 
-          echo "<div><img src='" . APIEndPoint . "preview" . ( EnableAPIAsSubDomain ? "?" : "&" ) . "id=" . FileInfo["FileID"] . "&type=" . $Ext . "' style='height:400px;width:auto;'></div><br>";
-
         }
 
+      } else if (
+          $Ext == "txt" || $Ext == "sql" || $Ext == "php" || $Ext == "html" ||
+          $Ext == "cgi" || $Ext == "cs" || $Ext == "c" || $Ext == "py" ||
+          $Ext == "js" || $Ext == "json" || $Ext == "css" || $Ext == "csv" ||
+          $Ext == "rs" || $Ext == "cpp" ) {
+
+        $Ext = "txt";
+
+        $tempData = sys_get_temp_dir() . "/end2endtech_" . FileInfo["FileID"];
+        if ( $FileSize > 1024 ) {
+          file_put_contents( $tempData, substr( gzinflate( file_get_contents( $basepath . FileInfo["FileID"] ) ) , 0, 1024 ) . "\n\n(1025バイト以降は省略されました。続きはダウンロードしてご確認下さい。)" );
+        } else {
+          file_put_contents( $tempData, gzinflate( file_get_contents( $basepath . FileInfo["FileID"] ) ) );
+        }
+
+        echo "<iframe style='width:604px;height:340px;' src='" . APIEndPoint . "preview" . ( EnableAPIAsSubDomain ? "?" : "&" ) . "id=" . FileInfo["FileID"] . "&type=" . $Ext . "'></iframe><br>";
+
       }
-
-    } else if (
-        $Ext == "txt" || $Ext == "sql" || $Ext == "php" || $Ext == "html" ||
-        $Ext == "cgi" || $Ext == "cs" || $Ext == "c" || $Ext == "py" ||
-        $Ext == "js" || $Ext == "json" || $Ext == "css" || $Ext == "csv" ||
-        $Ext == "rs" || $Ext == "cpp" ) {
-
-      $Ext = "txt";
-
-      $tempData = sys_get_temp_dir() . "/end2endtech_" . FileInfo["FileID"];
-      if ( $FileSize > 1024 ) {
-        file_put_contents( $tempData, substr( gzinflate( file_get_contents( $basepath . FileInfo["FileID"] ) ) , 0, 1024 ) . "\n\n(1025バイト以降は省略されました。続きはダウンロードしてご確認下さい。)" );
-      } else {
-        file_put_contents( $tempData, gzinflate( file_get_contents( $basepath . FileInfo["FileID"] ) ) );
-      }
-
-      echo "<iframe style='width:604px;height:340px;' src='" . APIEndPoint . "preview" . ( EnableAPIAsSubDomain ? "?" : "&" ) . "id=" . FileInfo["FileID"] . "&type=" . $Ext . "'></iframe><br>";
 
     }
 
